6b25330cba4cbb0501af964bba2960e6dfdd77f4
bovkun <alexandr05@list.ru>
2019-12-11 17:46:41 +0200

    Modified Node class

--- view/node.cpp
+++ view/node.cpp
@@ -1,20 +1,122 @@
 #include "node.h"
 #include "link.h"
 
+#include <QFontMetricsF>
+#include <QApplication>
+
 namespace dia {
 
 Node::Node()
-{  }
+	: _textColor(Qt::darkGreen)
+	, _backgroundColor(Qt::white)
+	, _outlineColor(Qt::darkBlue)
+{
+	setFlags(ItemIsMovable | ItemIsSelectable);
+}
 
 Node::~Node()
-{  }
+{
+//	foreach(Link* link, _pLinks) {
+//		delete link;
+//	}
+	for (Link* link: _pLinks) {
+		delete link;
+	}
+	_pLinks.clear();
+}
+
+void Node::setText(const QString& text)
+{
+	prepareGeometryChange();
+	_text = text;
+	update();
+}
+
+QString Node::text() const
+{
+	return _text;
+}
+
+void Node::setTextColor(const QColor& color)
+{
+	_textColor = color;
+	update();
+}
+
+QColor Node::textColor() const
+{
+	return _textColor;
+}
+
+void Node::setOutlineColor(const QColor& color)
+{
+	_outlineColor = color;
+	update();
+}
+
+QColor Node::outlineColor() const
+{
+	return _outlineColor;
+}
+
+void Node::setBackgroundColor(const QColor color)
+{
+	_backgroundColor = color;
+	update();
+}
+
+QColor Node::backgroundColor() const
+{
+	return _backgroundColor;
+}
 
 void Node::addLink(Link* link)
+{
+	_pLinks.insert(link);
+}
+
+void Node::removeLink(Link* link)
+{
+	_pLinks.remove(link);
+}
+
+QRectF Node::boundingRect() const
 {
 
 }
 
-void Node::removeLink(Link* link)
+QPainterPath Node::shape() const
+{
+
+}
+
+void Node::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
+{
+
+}
+
+void Node::mouseDoubleClickEvent(QGraphicsSceneMouseEvent* event)
+{
+
+}
+
+QVariant Node::itemChange(QGraphicsItem::GraphicsItemChange changed, const QVariant& value)
+{
+
+}
+
+QRectF Node::outlineRect() const
+{
+	constexpr int PADDING = 8;
+
+	QFontMetricsF metrics(qApp->font());
+	QRectF rect = metrics.boundingRect(_text);
+	rect.adjust(-PADDING, -PADDING, PADDING, PADDING);
+	rect.translate(-rect.center());
+	return rect;
+}
+
+int Node::roundness(double size) const
 {
 
 }
--- view/node.h
+++ view/node.h
@@ -2,6 +2,8 @@
 #define NODE_H
 
 #include <QGraphicsItem>
+#include <QCoreApplication>
+#include <QSet>
 
 namespace dia {
 class Link;
@@ -11,13 +13,51 @@ namespace dia {
 
 class Node : public QGraphicsItem
 {
-public:
-	Node();
-	~Node();
+	Q_DECLARE_TR_FUNCTIONS(Node)
 
 public:
+	Node();
+	~Node() override;
+
+public:
+	void setText(const QString& text);
+	QString text() const;
+
+	void setTextColor(const QColor& color);
+	QColor textColor() const;
+
+	void setOutlineColor(const QColor& color);
+	QColor outlineColor() const;
+
+	void setBackgroundColor(const QColor color);
+	QColor backgroundColor() const;
+
 	void addLink(Link* link);
 	void removeLink(Link* link);
+
+	QRectF boundingRect() const override;
+
+	QPainterPath shape() const override;
+
+	void paint(QPainter* painter,
+			   const QStyleOptionGraphicsItem* option,
+			   QWidget* widget) override;
+
+protected:
+	void mouseDoubleClickEvent(QGraphicsSceneMouseEvent* event) override;
+	QVariant itemChange(GraphicsItemChange changed,
+						const QVariant& value) override;
+
+private:
+	QRectF outlineRect() const;
+	int roundness(double size) const;
+
+private:
+	QSet<Link*> _pLinks;
+	QString     _text;
+	QColor      _textColor;
+	QColor      _backgroundColor;
+	QColor      _outlineColor;
 };
 
 } // namespace dia
